7d11e31436a9722ed09dccfdb0b0106de0d34482
alex <lopachyov@gmail.com>
2021-08-16 12:58:24 +0300

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn My First React
         </a>
       </header>
     </div>
