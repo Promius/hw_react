3d552ff25e3153b92684d341d800b64c51c7dd26
alex <lopachyov@gmail.com>
2021-08-26 17:19:44 +0300

    lesson_2

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import MessageList from './MessageList';
+import MessageInput from './MessageInput';
+import './App.scss';
+
+
+function App() {
+
+  const [inputMessage, setInputMessage] = useState('');
+  const [messagesArray, setMessagesArray] = useState([]);
+  const [botMessagesArray, setBotMessagesArray] = useState([]);
+
+  const onSendMessage = () => {
+    const trimmedMessageText = inputMessage.trim();
+
+    if (trimmedMessageText !== '') {
+      setMessagesArray(prev => [...prev,
+      {
+        trimmedMessageText,
+        author: 'Alex',
+      },
+      ]);
+      setInputMessage('');
+    };
+  };
+
+  useEffect(() => {
+    if (messagesArray.length > 0) {
+      setTimeout(() => {
+        console.log('Сообщение отправлено!');
+
+        setBotMessagesArray(() => [
+          {
+            botMessage: 'Сообщение отправлено!',
+            author: 'Chat-bot',
+          },
+        ]);
+      }, 1500);
+    };
+  }, [setBotMessagesArray, messagesArray]);
+
+  return <div className='mainWrapper'>
+
+    <MessageList messagesArray={messagesArray} botMessagesArray={botMessagesArray} />
+
+    <MessageInput inputMessage={inputMessage} setInputMessage={setInputMessage} onSendMessage={onSendMessage} />
+
+  </div >
+};
+export default App;
